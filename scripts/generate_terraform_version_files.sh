e692c528a6c4d489db3f8dd5f63c13b9a6603681
takapg <75102730+takapg@users.noreply.github.com>
2023-12-03 23:13:21 +0900

    Update generate_terraform_version_files.sh

--- scripts/generate_terraform_version_files.sh
+++ scripts/generate_terraform_version_files.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-generate_terraform_version() {
-  working_dir=$1
-  hcledit --file ${working_dir} attribute get terraform.required_version |
-  tr -d '"' > $(dirname ${working_dir})/.terraform-version
+generate_terraform_version_file() {
+  version_file_path=$1
+  hcledit --file ${version_file_path} attribute get terraform.required_version |
+  tr -d '"' > $(dirname ${version_file_path})/.terraform-version
 }
-export -f generate_terraform_version
+export -f generate_terraform_version_file
 
 find accounts -name version.tf |
-xargs -I{} bash -c 'generate_terraform_version {}'
+xargs -I{} bash -c 'generate_terraform_version_file {}'
