623f7117f885dc08c8c04871790ba26d6d5cffc3
takapg <75102730+takapg@users.noreply.github.com>
2023-12-03 21:31:31 +0900

    Update update_version_json.sh

--- scripts/update_version_json.sh
+++ scripts/update_version_json.sh
@@ -2,42 +2,46 @@
 
 set -eux
 
-TERRAFORM_REQUIRED_VERSION=$(
-  cat files/terraform/version.json |
-  jq -r '.terraform[0].required_version'
+REQUIRED_VERSION=$(
+  cat files/terraform/version.tf |
+  hcledit attribute get terraform.required_version
 )
 
-find accounts -name .terraform.lock.hcl |
-xargs -I{} hcl2json {} |
-jq -c '
-  .provider |
-  to_entries |
-  .[] |
-  {
-    name: (.key | capture("^.+/(?<name>.+)$") | .name),
-    source: .key,
-    version: .value[0].version
-  }
-' |
-jq -s -c '
-  group_by(.source) |
-  .[] |
-  sort_by(.version | split(".") | map(tonumber)) |
-  .[-1] |
-  {
-    (.name): {
-      source: .source,
-      version: .version
+REQUIRED_PROVIDERS=$(
+  find accounts -name .terraform.lock.hcl |
+  xargs -I{} hcl2json {} |
+  jq -c '
+    .provider |
+    to_entries |
+    .[] |
+    {
+      name: (.key | capture("^.+/(?<name>.+)$") | .name),
+      source: .key,
+      version: .value[0].version
     }
+  ' |
+  jq -s -r '
+    group_by(.source) |
+    .[] |
+    sort_by(.version | split(".") | map(tonumber)) |
+    .[-1] |
+    [
+      .name + " = {",
+      "source = \"" + .source + "\"",
+      "version = \"" + .version + "\"",
+      "}"
+    ] |
+    join("\n")
+  '
+)
+
+cat << EOF > files/terraform/version.tf
+terraform {
+  required_version = ${REQUIRED_VERSION}
+  required_providers {
+    ${REQUIRED_PROVIDERS}
   }
-' | 
-jq -s -r "
-  {
-    terraform: [
-      {
-        required_version: \"${TERRAFORM_REQUIRED_VERSION}\",
-        required_providers: .
-      }
-    ]
-  }
-" > files/terraform/version.json
+}
+EOF
+
+hcledit fmt -u -f files/terraform/version.tf
