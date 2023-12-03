934ee8881cb69c2d830c6cd5f72a39452173e1c3
takapg <75102730+takapg@users.noreply.github.com>
2023-12-03 21:43:55 +0900

    Update update_terraform_lock_hcl.sh

--- scripts/update_terraform_lock_hcl.sh
+++ scripts/update_terraform_lock_hcl.sh
@@ -3,7 +3,7 @@
 set -eux
 
 find accounts -name .terraform.lock.hcl |
-xargs -I{} awk -i inplace '/^#/' {}
+xargs -I{} bash -c 'awk "/^#/" {} > {}_tmp && mv {}_tmp {}'
 
 tfupdate lock \
   --platform=linux_amd64 --platform=linux_arm64 --platform=darwin_amd64 --platform=darwin_arm64 \
