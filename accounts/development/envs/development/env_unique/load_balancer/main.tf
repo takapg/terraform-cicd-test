77ac4ede613acd311ec22ad4cb8fb4b45ad35ef3
takapg <75102730+takapg@users.noreply.github.com>
2023-12-05 00:08:42 +0900

    Update main.tf

--- accounts/development/envs/development/env_unique/load_balancer/main.tf
+++ accounts/development/envs/development/env_unique/load_balancer/main.tf
@@ -1,6 +1,9 @@
 resource "null_resource" "a" {
 }
 
+resource "null_resource" "a" {
+}
+
 data "tls_certificate" "github_actions" {
   url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
 }
