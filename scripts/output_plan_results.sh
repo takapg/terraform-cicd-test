8c15e8a48e241b383aabe289fb526a08945c1211
takapg <75102730+takapg@users.noreply.github.com>
2023-12-16 23:40:49 +0900

    Update output_plan_results.sh (#2)
    * Update output_plan_results.sh
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * chore: Terraform PR check (check-code)
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update main.tf
    
    * chore: Terraform PR check (check-code)
    
    * Update terraform-pr-check.yml
    
    * Update aqua.yaml
    
    * Update output_plan_results.sh
    
    * Update output_plan_results.sh
    
    * Create github-comment.yaml
    
    * Update aqua.yaml
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    * Update github-comment.yaml
    
    * Update output_plan_results.sh
    
    * Update output_plan_results.sh
    
    * Update output_plan_results.sh
    
    * Update output_plan_results.sh
    
    * Update terraform-pr-check.yml
    
    * Update terraform-pr-check.yml
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- scripts/output_plan_results.sh
+++ scripts/output_plan_results.sh
@@ -7,6 +7,7 @@ tmp_tfcmt_result_file_name='tmp_tfcmt_result.md'
 plan_results_file_name='plan_results.md'
 
 plan_actions_regex_pattern='^Plan: [0-9]+ to add,.+to destroy\.$'
+ci_link_regex_pattern='^\[CI link\].+$'
 
 diff_results=''
 no_diff_results=''
@@ -31,3 +32,63 @@ rm_tmp_files() {
 rm_tmp_files
 
 terragrunt run-all plan --terragrunt-tfpath $(git rev-parse --show-toplevel)/scripts/terraform_with_output_log_file.sh
+
+for tmp_tf_log_file_path in $(find . -name ${tmp_tf_log_file_name} | sort); do
+  root_module_dir=$(dirname ${tmp_tf_log_file_path})
+  root_module_dir_to_git_repo_root="$(git rev-parse --show-prefix)$(echo ${root_module_dir} | sed 's/.\///')"
+  tmp_tfcmt_result_file_path="${root_module_dir}/${tmp_tfcmt_result_file_name}"
+
+  echo ''
+  echo ''
+  echo '###'
+  echo "# ${root_module_dir_to_git_repo_root}"
+  echo '###'
+
+  tfcmt --output ${tmp_tfcmt_result_file_path} plan -- cat ${tmp_tf_log_file_path}
+
+  result="
+  <tr>
+    <td>${root_module_dir_to_git_repo_root}</td>
+    <td>$(cat ${tmp_tfcmt_result_file_path} | sed 's/## Plan Result//')</td>
+  </tr>
+  "
+
+  plan_actions_text=$(cat ${tmp_tfcmt_result_file_path} | grep -E "${plan_actions_regex_pattern}" || echo '')
+
+  if [ "${plan_actions_text}" != '' ]; then
+    diff_results+="${result}"
+  else
+    no_diff_results+="${result}"
+  fi
+done
+
+plan_results="
+### Diff results
+
+<table>
+  ${result_header}
+  ${diff_results}
+</table>
+
+---
+
+### No diff results
+
+<details><summary>(Click me)</summary>
+  <table>
+    ${result_header}
+    ${no_diff_results}
+  </table>
+</details>
+"
+
+ci_link=$(echo "${plan_results}" | grep -E "${ci_link_regex_pattern}" | uniq)
+plan_results_without_ci_link=$(echo "${plan_results}" | grep -v -E "${ci_link_regex_pattern}")
+
+cat << EOF > ${plan_results_file_name}
+${ci_link}
+
+${plan_results_without_ci_link}
+EOF
+
+rm_tmp_files
