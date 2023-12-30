816d6a13c6ad9e515da12885be59710d3917beda
takapg <75102730+takapg@users.noreply.github.com>
2023-12-31 00:20:19 +0900

    Update version.tf

--- files/terraform/version.tf
+++ files/terraform/version.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = "1.6.6"
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "2.4.1"
-    }
     null = {
       source  = "hashicorp/null"
       version = "3.2.2"
