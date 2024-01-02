cd095f691152c8f201bde08359f3528ff0ea83ff
takapg <75102730+takapg@users.noreply.github.com>
2024-01-02 23:10:07 +0900

    Update scripts_spec.sh

--- spec/scripts_spec.sh
+++ spec/scripts_spec.sh
@@ -7,7 +7,6 @@ BeforeEach 'setup_each_tmpdir'
 
 Describe 'aaa'
   It 'should be success'
-    echo ${EACH_TMPDIR}
     When call echo 'aaa'
     The output should equal 'aaa'
   End
@@ -27,18 +26,16 @@ Describe 'generate_terraform_version_files.sh'
       #|1.0.0
     )
 
-    tmp_root_path='./spec/tmp/generate_terraform_version_files'
-    mkdir -p "${tmp_root_path}/target_01"
-    mkdir -p "${tmp_root_path}/target_02"
-    mkdir -p "${tmp_root_path}/not_target_01"
+    mkdir -p "${EACH_TMPDIR}/target_01"
+    mkdir -p "${EACH_TMPDIR}/target_02"
+    mkdir -p "${EACH_TMPDIR}/not_target_01"
 
-    echo "${version_tf}" > "${tmp_root_path}/target_01/version.tf"
-    echo "${version_tf}" > "${tmp_root_path}/target_02/version.tf"
+    echo "${version_tf}" > "${EACH_TMPDIR}/target_01/version.tf"
+    echo "${version_tf}" > "${EACH_TMPDIR}/target_02/version.tf"
 
-    echo ${EACH_TMPDIR}
-    When call ./scripts/generate_terraform_version_files.sh ${tmp_root_path}
-    The contents of file "${tmp_root_path}/target_01/.terraform-version" should equal "${dot_terraform_version}"
-    The contents of file "${tmp_root_path}/target_02/.terraform-version" should equal "${dot_terraform_version}"
-    The path "${tmp_root_path}/not_target_01/.terraform-version" should not be exist
+    When call ./scripts/generate_terraform_version_files.sh ${EACH_TMPDIR}
+    The contents of file "${EACH_TMPDIR}/target_01/.terraform-version" should equal "${dot_terraform_version}"
+    The contents of file "${EACH_TMPDIR}/target_02/.terraform-version" should equal "${dot_terraform_version}"
+    The path "${EACH_TMPDIR}/not_target_01/.terraform-version" should not be exist
   End
 End
