1a724f0e36bc9839df5db38c0049dd04101b20ce
takapg <75102730+takapg@users.noreply.github.com>
2024-01-03 01:50:26 +0900

    Update generate_terraform_version_files.sh

--- scripts/generate_terraform_version_files.sh
+++ scripts/generate_terraform_version_files.sh
@@ -4,12 +4,12 @@ set -eu
 
 root_modules_top_dir=$1
 
-generate_terraform_version_file() {
-  version_file_path=$1
-  hcledit --file ${version_file_path} attribute get terraform.required_version \
-    | tr -d '"' > $(dirname ${version_file_path})/.terraform-version
+generate_dot_terraform_version_file() {
+  version_tf_path=$1
+  hcledit --file ${version_tf_path} attribute get terraform.required_version \
+    | tr -d '"' > $(dirname ${version_tf_path})/.terraform-version
 }
-export -f generate_terraform_version_file
+export -f generate_dot_terraform_version_file
 
 find ${root_modules_top_dir} -name version.tf \
-  | xargs -I{} bash -c 'generate_terraform_version_file {}'
+  | xargs -I{} bash -c 'generate_dot_terraform_version_file {}'
