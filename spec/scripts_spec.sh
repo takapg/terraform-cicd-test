af858dc25dbf23d8d855d5ceb66493b175c89f30
takapg <75102730+takapg@users.noreply.github.com>
2024-01-02 13:50:27 +0900

    Update scripts_spec.sh

--- spec/scripts_spec.sh
+++ spec/scripts_spec.sh
@@ -1,6 +1,30 @@
+setup() {
+  mkdir -p ./spec/tmp
+}
+
+cleanup() {
+  rm -rf ./spec/tmp
+}
+
+BeforeAll 'setup'
+AfterAll 'cleanup'
+
 Describe 'aaa'
   It 'should be success'
     When call echo 'aaa'
+    The output should equal 'aaa'
+  End
+End
+
+Describe 'generate_terraform_version_files.sh'
+  It 'should be success'
+    version_tf=$(
+      %text
+      #|terraform {
+      #|  required_version = "1.0.0"
+      #|}
+    )
+    When call echo ${version_tf}
     The output should equal 'aaa'
   End
 End
