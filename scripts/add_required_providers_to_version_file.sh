e1cdec0ab5603b88da2de5ae1e1c9b1101340e3d
takapg <75102730+takapg@users.noreply.github.com>
2024-01-03 03:44:44 +0900

    Update add_required_providers_to_version_file.sh

--- scripts/add_required_providers_to_version_file.sh
+++ scripts/add_required_providers_to_version_file.sh
@@ -6,36 +6,36 @@ root_modules_top_dir=$1
 template_version_tf_path=$2
 
 REQUIRED_VERSION=$(
-  cat ${template_version_tf_path} |
-  hcledit attribute get terraform.required_version
+  cat ${template_version_tf_path} \
+    | hcledit attribute get terraform.required_version
 )
 
 REQUIRED_PROVIDERS=$(
-  find ${root_modules_top_dir} -name .terraform.lock.hcl |
-  xargs -I{} hcl2json {} |
-  jq -c '
-    .provider |
-    to_entries |
-    .[] |
-    {
-      name: (.key | capture("^.+/(?<name>.+)$") | .name),
-      source: (.key | capture("^registry\\.terraform\\.io/(?<source>.+)$") | .source),
-      version: .value[0].version
-    }
-  ' |
-  jq -s -r '
-    group_by(.source) |
-    .[] |
-    sort_by(.version | split(".") | map(tonumber)) |
-    .[-1] |
-    [
-      .name + " = {",
-      "source = \"" + .source + "\"",
-      "version = \"" + .version + "\"",
-      "}"
-    ] |
-    join("\n")
-  '
+  find ${root_modules_top_dir} -name .terraform.lock.hcl \
+    | xargs -I{} hcl2json {} \
+    | jq -c '
+      .provider |
+      to_entries |
+      .[] |
+      {
+        name: (.key | capture("^.+/(?<name>.+)$") | .name),
+        source: (.key | capture("^registry\\.terraform\\.io/(?<source>.+)$") | .source),
+        version: .value[0].version
+      }
+    ' \
+    | jq -s -r '
+      group_by(.source) |
+      .[] |
+      sort_by(.version | split(".") | map(tonumber)) |
+      .[-1] |
+      [
+        .name + " = {",
+        "source = \"" + .source + "\"",
+        "version = \"" + .version + "\"",
+        "}"
+      ] |
+      join("\n")
+    '
 )
 
 cat << EOF > ${template_version_tf_path}
