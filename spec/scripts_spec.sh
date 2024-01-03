656621be220cb9b99cdd8f11aaeb5ff3bd799973
takapg <75102730+takapg@users.noreply.github.com>
2024-01-03 21:44:24 +0900

    Update scripts_spec.sh

--- spec/scripts_spec.sh
+++ spec/scripts_spec.sh
@@ -131,3 +131,60 @@ Describe 'add_required_providers_to_template_version_tf_file.sh'
     The contents of file ./files/version.tf should equal "${expected_template_version_tf_contents}"
   End
 End
+
+Describe 'update_dependency_lock_files.sh'
+  It 'should be success'
+    fixture_version_tf_contents=$(
+      %text
+      #|terraform {
+      #|  required_version = "1.0.0"
+      #|  required_providers {
+      #|    null = {
+      #|      source  = "hashicorp/null"
+      #|      version = "3.2.2"
+      #|    }
+      #|    tls = {
+      #|      source  = "hashicorp/tls"
+      #|      version = "4.0.5"
+      #|    }
+      #|  }
+      #|}
+    )
+
+    mkdir -p ./top/target_01
+    mkdir -p ./top/target_02
+    mkdir -p ./top/not_target_01
+
+    echo "${fixture_version_tf_contents}" > ./top/target_01/version.tf
+    echo "${fixture_version_tf_contents}" > ./top/target_02/version.tf
+
+    expected_dot_terraform_lock_hcl_contents=$(
+      %text
+      #|# This file is maintained automatically by "terraform init".
+      #|# Manual edits may be lost in future updates.
+      #|
+      #|provider "registry.terraform.io/hashicorp/c" {
+      #|  version     = "1.3.0"
+      #|  constraints = "1.3.0"
+      #|  hashes = [
+      #|    "h1:dummy",
+      #|    "zh:dummy",
+      #|  ]
+      #|}
+      #|
+      #|provider "registry.terraform.io/hashicorp/b" {
+      #|  version     = "1.2.0"
+      #|  constraints = "1.2.0"
+      #|  hashes = [
+      #|    "h1:dummy",
+      #|    "zh:dummy",
+      #|  ]
+      #|}
+    )
+
+    When run script update_dependency_lock_files.sh ./top
+    The contents of file ./top/target_01/.terraform.lock.hcl should equal "${expected_dot_terraform_lock_hcl_contents}"
+    The contents of file ./top/target_02/.terraform.lock.hcl should equal "${expected_dot_terraform_lock_hcl_contents}"
+    The path ./top/not_target_01/.terraform.lock.hcl should not be exist
+  End
+End
